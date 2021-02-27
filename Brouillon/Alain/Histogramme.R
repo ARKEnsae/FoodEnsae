45b9b9a2cc55c741fc1d172dec2f28b2e03e676c
AQLT <alain.quartier@yahoo.fr>
2021-02-27 19:33:20 +0100

    update alain

--- Brouillon/Alain/Histogramme.R
+++ Brouillon/Alain/Histogramme.R
@@ -16,6 +16,9 @@ library(highcharter)
 # data = data[(data$brands!="")&(data$nutriscore_grade!=""),]
 # write.table(data,file = "data/sub_data.csv",fileEncoding="UTF-8", sep="\t")
 data <- data.table::fread("data/sub_data.csv", encoding = "UTF-8")
+head(data$generic_name)
+unique(data$generic_name)
+head(data$product_name)
 unique(data$nutriscore_grade)
 colnames(donnees)
 donnees$brands_tags
@@ -27,6 +30,14 @@ donnees$nutriscore_grade
 # ggplot(donnees, aes(x=`nutriscore_grade`)) + 
 #   geom_bar()
 
+#Color
+dput(c(rgb(56, 126, 72, names = "A", maxColorValue = 255),
+       rgb(145, 184, 73, names = "B", maxColorValue = 255),
+       rgb(247, 203, 70, names = "C", maxColorValue = 255),
+       rgb(224, 133, 49, names = "D", maxColorValue = 255),
+       rgb(213, 76, 41, names = "E", maxColorValue = 255)))
+colors_grade = c(A = "#387E48", B = "#91B849", C = "#F7CB46", D = "#E08531", 
+  E = "#D54C29")
 
 ggplot(data, aes(x=`nutriscore_grade`)) + 
   geom_bar()
@@ -46,8 +57,10 @@ brand_grade <- data %>%
   count(brands,`nutriscore_grade`) %>% 
   arrange(-n)
 brand_grade_t15 <- brand_grade[brand_grade$brands%in%head(unique(brand_grade$brands),15),]
+brand_grade_t15$nutriscore_grade <- toupper(brand_grade_t15$nutriscore_grade)
 unique(brand_grade_t15$brands)
 unique(brand_grade_t15$nutriscore_grade)
+c(56,66)
 
 hchart(
   brand_grade_t15, 
@@ -58,9 +71,14 @@ hchart(
     style = list(fontFamily = "Gloria Hallelujah")
   ) %>% 
   hc_title(
-    text = "Nutriscore des produits des principales marques",
+    text = "Nombre de produits par nutriscore des principales marques",
     style = list(fontFamily = "Gloria Hallelujah")
-  ) 
+  ) %>% 
+  hc_yAxis(
+    title = list(text="Nombre de produits"))%>% 
+  hc_xAxis(
+    title = list(text=""))
+
 hchart(brand_grade_t15, "treemap",
        hcaes(x = brands, value = n, color = nutriscore_grade))%>% 
   hc_chart(
@@ -68,11 +86,22 @@ hchart(brand_grade_t15, "treemap",
   ) 
 
 
+data_plot <- data_to_hierarchical(brand_grade_t15, c(brands,nutriscore_grade),n)
+x = data_plot[[1]]
+data_plot2 <- lapply(data_plot,function(x){
+  if(x$level==1){
+    x$showInLegend = FALSE
+    return(x)
+  }else{
+    x$color = as.character(colors_grade[x$name])
+    return(x)
+  }
+})
 lvl_opts <-  list(
   list(
     level = 1,
-    borderWidth = 0,
-    borderColor = "transparent",
+    borderWidth = 4,
+    borderColor = "black",
     dataLabels = list(
       enabled = TRUE,
       align = "left",
@@ -83,19 +112,21 @@ lvl_opts <-  list(
   list(
     level = 2,
     borderWidth = 0,
-    borderColor = "transparent",
-    colorVariation = list(key = "brightness", to = 0.250),
-    dataLabels = list(enabled = FALSE),
+    borderColor = "white",
+    # colorVariation = list(key = "brightness", to = 0.250),
+    dataLabels = list(enabled = FALSE), # pour ne pas afficher legend sur graph
     style = list(fontSize = "8px", textOutline = FALSE, color = "white")
   )
 )
 hchart(
-  data_to_hierarchical(brand_grade_t15, c(brands,nutriscore_grade), n),
+  data_plot2,
   type = "treemap",
   # levelIsConstant = FALSE,
   # allowDrillToNode = TRUE,
   levels = lvl_opts,
-  tooltip = list(valueDecimals = FALSE)
+  tooltip = list(valueDecimals = FALSE),
+  showInLegend = FALSE,
+  legendType= 'point'
 ) %>% 
   hc_chart(
     style = list(fontFamily = "Gloria Hallelujah")
@@ -103,7 +134,7 @@ hchart(
   hc_title(
     text = "Nutriscore des produits des principales marques",
     style = list(fontFamily = "Gloria Hallelujah")
-  ) 
+  )
 hchart(brand_grade_t15, "treemap",
        hcaes(x = brands, value = n, color = nutriscore_grade))%>% 
   hc_chart(
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Data_Storytelling
 Notre projet de 3A du cours de Data storytelling @antuki @AQLT
+
+
+````
+dput(c(rgb(56, 126, 72, names = "A", maxColorValue = 255),
+       rgb(145, 184, 73, names = "B", maxColorValue = 255),
+       rgb(247, 203, 70, names = "C", maxColorValue = 255),
+       rgb(224, 133, 49, names = "D", maxColorValue = 255),
+       rgb(213, 76, 41, names = "E", maxColorValue = 255)))
+c(A = "#387E48", B = "#91B849", C = "#F7CB46", D = "#E08531", 
+  E = "#D54C29")
+```
