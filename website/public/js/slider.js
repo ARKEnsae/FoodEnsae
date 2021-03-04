3e0714ffcfa0670cfd6c90f729fefa2e219bda3a
antuki <kim.antunez@ensae.fr>
2021-03-04 16:49:35 +0100

    04/03 : suite tests slider...

--- website/public/js/slider.js
+++ website/public/js/slider.js
@@ -78,7 +78,37 @@ function startTimer(){
 }
 
 // Mise en oeuvre du "responsive"
-window.addEventListener("resize", () => {
-    slideWidth = diapo.getBoundingClientRect().width
-    slideNext()
-})
+//window.addEventListener("resize", () => {
+//    slideWidth = diapo.getBoundingClientRect().width
+//    slideNext()
+//})
+
+
+// tests
+
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+function plusSlides2(n) {
+  /slideIndex2 = slideIndex2 + n;
+..   showSlides2(slideIndex2); 
+  showSlides2(slideIndex2 += n);
+}
+
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+  var i;
+  var slides = document.getElementsByClassName("element");
+  if (n > slides.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+     // slides[i].style.display = "block";
+      
+  }
+  slides[slideIndex2-1].style.display = "block";
+  //slides[slideIndex2-1].className += " active"; //NEW
+} 
--- website/static/js/slider.js
+++ website/static/js/slider.js
@@ -1,84 +1,27 @@
-// On récupère le conteneur principal du diaporama
-const diapo = document.querySelector(".diapo")
 
-// On récupère le conteneur de tous les éléments
-elements = document.querySelector(".elements")
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
 
-// On récupère un tableau contenant la liste des diapos
-slides = Array.from(elements.children)
-
-// On récupère les deux flèches
-let next = document.querySelector("#nav-droite")
-let prev = document.querySelector("#nav-gauche")
-
-// Variables globales
-let compteur = 0 // Compteur qui permettra de savoir sur quelle slide nous sommes
-let timer, elements, slides, slideWidth
-
-// On calcule la largeur visible du diaporama
-slideWidth = diapo.getBoundingClientRect().width
-
-// On met en place les écouteurs d'évènements sur les flèches
-next.addEventListener("click", slideNext)
-prev.addEventListener("click", slidePrev)
-
-/**
- * Cette fonction fait défiler le diaporama vers la droite
- */
-function slideNext(){
-    // On incrémente le compteur
-    compteur++
-
-    // Si on dépasse la fin du diaporama, on "rembobine"
-    if(compteur == slides.length){
-        compteur = 0
-    }
-
-    // On calcule la valeur du décalage
-    let decal = -slideWidth * compteur
-    elements.style.transform = `translateX(${decal}px)`
+function plusSlides2(n) {
+//  slideIndex2 = slideIndex2 + n;
+//   showSlides2(slideIndex2); 
+  showSlides2(slideIndex2 += n);
 }
 
-/**
- * Cette fonction fait défiler le diaporama vers la gauche
- */
-function slidePrev(){
-    // On décrémente le compteur
-    compteur--
-
-    // Si on dépasse le début du diaporama, on repart à la fin
-    if(compteur < 0){
-        compteur = slides.length - 1
-    }
-
-    // On calcule la valeur du décalage
-    let decal = -slideWidth * compteur
-    elements.style.transform = `translateX(${decal}px)`
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
 }
 
-// Automatiser le diaporama
-timer = setInterval(slideNext, 4000)
-
-// Gérer le survol de la souris
-diapo.addEventListener("mouseover", stopTimer)
-diapo.addEventListener("mouseout", startTimer)
-
-/**
- * On stoppe le défilement
- */
-function stopTimer(){
-    clearInterval(timer)
-}
-
-/**
- * On redémarre le défilement
- */
-function startTimer(){
-    timer = setInterval(slideNext, 4000)
-}
-
-// Mise en oeuvre du "responsive"
-window.addEventListener("resize", () => {
-    slideWidth = diapo.getBoundingClientRect().width
-    slideNext()
-})
+function showSlides2(n) {
+  var i;
+  var slides = document.getElementsByClassName("element");
+  if (n > slides.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+     // slides[i].style.display = "block";
+      
+  }
+  slides[slideIndex2-1].style.display = "block";
+  //slides[slideIndex2-1].className += " active"; //NEW
+} 
