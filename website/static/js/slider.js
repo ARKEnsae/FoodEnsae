94d5a4fdfe2aa7369e1045e561bf31ea29728215
antuki <kim.antunez@ensae.fr>
2021-03-04 12:47:03 +0100

    04/03 gauge color et position + slider suite mais pas fin...

--- website/static/js/slider.js
+++ website/static/js/slider.js
@@ -1,26 +1,84 @@
- var slideIndex = 1;
-showSlides(slideIndex);
+// On récupère le conteneur principal du diaporama
+const diapo = document.querySelector(".diapo")
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+// On récupère le conteneur de tous les éléments
+elements = document.querySelector(".elements")
+
+// On récupère un tableau contenant la liste des diapos
+slides = Array.from(elements.children)
+
+// On récupère les deux flèches
+let next = document.querySelector("#nav-droite")
+let prev = document.querySelector("#nav-gauche")
+
+// Variables globales
+let compteur = 0 // Compteur qui permettra de savoir sur quelle slide nous sommes
+let timer, elements, slides, slideWidth
+
+// On calcule la largeur visible du diaporama
+slideWidth = diapo.getBoundingClientRect().width
+
+// On met en place les écouteurs d'évènements sur les flèches
+next.addEventListener("click", slideNext)
+prev.addEventListener("click", slidePrev)
+
+/**
+ * Cette fonction fait défiler le diaporama vers la droite
+ */
+function slideNext(){
+    // On incrémente le compteur
+    compteur++
+
+    // Si on dépasse la fin du diaporama, on "rembobine"
+    if(compteur == slides.length){
+        compteur = 0
+    }
+
+    // On calcule la valeur du décalage
+    let decal = -slideWidth * compteur
+    elements.style.transform = `translateX(${decal}px)`
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+/**
+ * Cette fonction fait défiler le diaporama vers la gauche
+ */
+function slidePrev(){
+    // On décrémente le compteur
+    compteur--
+
+    // Si on dépasse le début du diaporama, on repart à la fin
+    if(compteur < 0){
+        compteur = slides.length - 1
+    }
+
+    // On calcule la valeur du décalage
+    let decal = -slideWidth * compteur
+    elements.style.transform = `translateX(${decal}px)`
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
+// Automatiser le diaporama
+timer = setInterval(slideNext, 4000)
+
+// Gérer le survol de la souris
+diapo.addEventListener("mouseover", stopTimer)
+diapo.addEventListener("mouseout", startTimer)
+
+/**
+ * On stoppe le défilement
+ */
+function stopTimer(){
+    clearInterval(timer)
+}
+
+/**
+ * On redémarre le défilement
+ */
+function startTimer(){
+    timer = setInterval(slideNext, 4000)
+}
+
+// Mise en oeuvre du "responsive"
+window.addEventListener("resize", () => {
+    slideWidth = diapo.getBoundingClientRect().width
+    slideNext()
+})
